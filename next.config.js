ebe42c7aec10c0d8e8662b7b61aa337e341d79aa
alaqsaka <aqsaabidin15@gmail.com>
2022-09-11 20:20:11 +0700

    integrate playlist api

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/signin",
+        destination: "/singin",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
