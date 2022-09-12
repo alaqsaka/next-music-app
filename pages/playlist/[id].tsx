ef34f5cbc73a6ede5c54a973cbd45208c45288de
alaqsaka <aqsaabidin15@gmail.com>
2022-09-12 08:49:32 +0700

    playlist page ui

---
+++ pages/playlist/[id].tsx
@@ -0,0 +1,66 @@
+import GradientLayout from "../../components/gradientLayout";
+import { validateToken } from "../../lib/auth";
+// import { NextRequest } from "next/server";
+import prisma from "../../lib/prisma";
+
+// random background
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
+const Playlist = ({ playlist }) => {
+  const color = getBGColor(playlist.id);
+
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <div>Hello</div>
+    </GradientLayout>
+  );
+};
+
+// get id from the params
+export const getServerSideProps = async ({ query, req }) => {
+  const { id } = validateToken(req.cookies.ACCESS_TOKEN);
+  const [playlist] = await prisma.playlist.findMany({
+    where: {
+      id: +query.id,
+      userId: id,
+    },
+    include: {
+      songs: {
+        include: {
+          artist: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return {
+    props: { playlist },
+  };
+};
+
+export default Playlist;
